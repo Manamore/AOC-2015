74d5b99aa58d0246e224f654dd18fc89636c6522
Manamore <not_you7@hotmail.com>
2017-12-17 16:32:22 +1000

    Day 3 pt2

--- day3/src/day3/Main.java
+++ day3/src/day3/Main.java
@@ -15,20 +15,33 @@ public class Main {
 	public static void main(String[] args) {
 		List<Character> input_arr = readFileByChar("../testdata/day3.txt");
 		Set<Point> coord_arr = new HashSet<>();
-		//List<Point> coord_arr = new ArrayList<>(); To make sure all values print as expected
-		//int counter = 1;
-		
-		Point coord_curr = new Point(0, 0);
+		//List<Point> coord_arr = new ArrayList<>(); //To make sure all values print as expected
+		int turn = 0; //turn 1 = santa, turn 2 = robo-santa
+
+		Point coord_curr_s = new Point(0, 0);
+		Point coord_curr_rs = new Point(0, 0);
 		//Prep position 0
-		coord_arr.add(coord_curr);
+		coord_arr.add(coord_curr_s);
 		//System.out.println("Counter [" + counter + "] " + "- : " + coord_curr.x + ", " + coord_curr.y);
 		
 		//Assign coords from input
 		for(char c : input_arr){
 			//counter += 1;
-			coord_curr = new Point(coord_curr.x, coord_curr.y);
+			Point coord_curr;
+			if(turn == 1) {
+				coord_curr_s = new Point(coord_curr_s.x, coord_curr_s.y);
+				turn -= 1;
+				coord_curr = coord_curr_s;
+			}
+			else {
+				coord_curr_rs = new Point(coord_curr_rs.x, coord_curr_rs.y);
+				turn += 1;
+				coord_curr = coord_curr_rs;
+			}
+				
 			double x = coord_curr.x;
 			double y = coord_curr.y;
+
 			switch (c){
 				case '>': 	coord_curr.setLocation(x += 1, y);
 							break;
@@ -45,8 +58,12 @@ public class Main {
 		}
 		
 		//Coords recorded as expected
+
+		int turn_test = 1;
 		/*for(Point p : coord_arr){
-			System.out.println(p.x + ", " + p.y);
+			System.out.println("Turn: " + turn_test + " Coords: " +p.x + ", " + p.y);
+			if (turn_test == 1) turn_test -= 1;
+			else turn_test += 1;
 		}*/
 		
 		//Print answer
