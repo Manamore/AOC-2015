a3152a9d4219782e5775960b3325ffbb0c2c1866
Manamore <not_you7@hotmail.com>
2018-01-03 14:48:42 +1000

    Day 5 Part 2

--- day5/src/day5/Main.java
+++ day5/src/day5/Main.java
@@ -3,7 +3,9 @@ package day5;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Main {
 
@@ -14,10 +16,11 @@ public class Main {
 		int naughty = 0;
 		int nice = 0;
 		for (String input : inputs){
-			if(!isNaughty(input) && isNice(input)){
-				nice += 1;
+			if(isNice(input)){
+				nice+=1;
 			}
-			else{
+			else
+			{
 				naughty += 1;
 			}
 		}
@@ -25,38 +28,44 @@ public class Main {
 		
 	}
 	
-	public static boolean isNaughty(String input){
-		String[] naughtyValues = {"ab", "cd", "pq", "xy"};
-		
-		for (String naughtyString : naughtyValues){
-			if(input.contains(naughtyString)){
-				return true;
-			}
-		}
-		return false;
-	}
-	
 	public static boolean isNice(String input){
-		char[] vowels = {'a', 'e', 'i', 'o', 'u'};
-		int vowelCounter = 0;
-		boolean doubleChar = false;
 		
-		for (int i = 0; i<=input.length() - 1; i++){
-			for (char vowel : vowels){
-				if (input.charAt(i) == vowel){
-					vowelCounter ++;
+		List<String> charPairArray = new ArrayList<String>();
+		boolean seperatedChars = false;
+				
+		for(int i = 0; i < input.length()-1; i ++){
+			//Grab pairs
+			charPairArray.add("" + input.charAt(i) + input.charAt(i+1));
+			//Prevent OOB
+			if(i != input.length() - 2){
+				//Catch all trips
+				if(input.charAt(i) == input.charAt(i+1) && input.charAt(i+1) == input.charAt(i+2)){
+					return false;
+				}
+				//Catch seperated xyx
+				if(input.charAt(i) == input.charAt(i+2)){
+					seperatedChars = true;
 				}
 			}
-			if(!doubleChar && i+1 != input.length() && input.charAt(i) == input.charAt(i+1)){ //catch final val to prevent index oob
-				doubleChar = true;
-			}
-			
-			if (doubleChar && vowelCounter >= 3){
-				return true;
-			}
 		}
-		return false;
+		
+		Set<String> charPairSet = new HashSet<String>(charPairArray);
+		/*System.out.print("\n " + input + " : ");
+		if(seperatedChars){
+			System.out.print("Enough seperation, ");
+		}
+		if(charPairArray.size() > charPairSet.size()){
+			System.out.print("Enough pairs.");
+		}*/
+		
+		//Check more than one pair of pair exists and there is seperated char
+		if(seperatedChars && charPairArray.size() > charPairSet.size()){
+			return true;
+		}
+		
 
+		
+		return false;
 	}
 	
 	public static List<String> readFileByLine(String filePath){
